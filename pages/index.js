fda87e96d31dc935c6620102f57800f34c3496c1
jonasserino <jonasserino@gmail.com>
2021-06-23 20:00:02 +0800

    y

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
+import Head from 'next/head'
 import NavBar from '../components/NavBar'
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Head from 'next/head'
 import HomeComponent from '../components/home'
 
 export default function Home() {
