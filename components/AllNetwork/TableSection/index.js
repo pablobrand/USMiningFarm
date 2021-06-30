027b8915de2a3ed86a68aefb5be3874e0935bb0e
jonasserino <jonasserino@gmail.com>
2021-06-30 23:07:52 +0800

    added dropdown in all-network page

--- components/AllNetwork/TableSection/index.js
+++ components/AllNetwork/TableSection/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/button-has-type */
 import Table from './Table'
+import Dropdown from '../Dropdown'
 
 const TableSection = () => {
     return (
@@ -17,14 +18,7 @@ const TableSection = () => {
                     <div className="col-md-6 col-xs-12 col-xs-12">
                         <div className="form-group network-dropdown">
                             <div className="dropdown">
-                                <button
-                                    className="text-white font-weight-bold btn dropdown-toggle"
-                                    type="button"
-                                    data-toggle="dropdown"
-                                >
-                                    ALL NETWORKS
-                                    <span className="caret" />
-                                </button>
+                                <Dropdown />
                                 <ul className="dropdown-menu">
                                     <li>
                                         <a href="#"> Lorem </a>
