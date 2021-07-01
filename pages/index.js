3b7660a32d8233f2fcc4a51292105c3e43a0f769
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2021-07-01 19:06:34 +0800

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import HomeComponent from '../components/Home'
+import HomeComponent from '../components/Homepage'
 import Footer from '../components/Footer'
 
 export default function Home() {
