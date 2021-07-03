407b3e7280b0583c5b88590c43fdf1b6a53ef319
jonasserino <jonasserino@gmail.com>
2021-07-03 10:12:35 +0800

    changed AllNetwork folder to LPStake

--- pages/all-network.js
+++ pages/all-network.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import AllNetworkComponent from '../components/AllNetwork'
+import AllNetworkComponent from '../components/LPStake'
 
 function AllNetwork() {
     return (
