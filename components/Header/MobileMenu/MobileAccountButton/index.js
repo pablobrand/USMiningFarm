9cb283343bc2ce9cdb110ceea704f28df93cb705
jonasserino <jonasserino@gmail.com>
2021-07-06 21:38:37 +0800

    converted mobile component to material UI

--- components/Header/MobileMenu/MobileAccountButton/index.js
+++ components/Header/MobileMenu/MobileAccountButton/index.js
@@ -1,16 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from 'next/link'
 import Image from 'next/image'
+import { Button, makeStyles } from '@material-ui/core'
 import { useMetaMask } from '../../../../functions/metamask/MetamaskContext'
 import styles from './AccountButton.module.css'
 
+const useStyles = makeStyles({
+    mobileAccountButtonStyles: {
+        width: '100%',
+        backgroundColor: '#3f3f3f',
+        color: '#fff',
+        padding: '10px 20px',
+        borderRadius: '50px',
+        textAlign: 'center'
+    }
+})
+
 const AccountButton = () => {
+    const classes = useStyles()
     const {
         metamaskState: { walletAccount }
     } = useMetaMask()
 
     return (
-        <div className={styles.accountButton}>
+        <Button className={classes.mobileAccountButtonStyles}>
             <Link href="#">
                 <a className={styles.accountLink}>
                     <Image src="/assets/images/account-icon.png" width={30} height={30} />
@@ -18,7 +31,7 @@ const AccountButton = () => {
                     {walletAccount && 'OCXA12........6665'}
                 </a>
             </Link>
-        </div>
+        </Button>
     )
 }
 
--- components/Header/MobileMenu/index.js
+++ components/Header/MobileMenu/index.js
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { Box } from '@material-ui/core'
+import { Box, Button, makeStyles } from '@material-ui/core'
 import styles from './MobileMenu.module.css'
 import MobileAccountButton from './MobileAccountButton'
 
+const useStyles = makeStyles({
+    active: {
+        width: '100%',
+        borderRadius: '10px',
+        backgroundImage: 'linear-gradient(to right, #b43838, #b8374d, #b83b61, #b54175, #ae4a87)',
+        color: '#fff',
+        textDecoration: 'none'
+    },
+
+    mobileNavItem: {
+        color: '#fff',
+        fontSize: '17px',
+        fontWeight: 600
+    }
+})
+
 const MobileMenu = () => {
+    const classes = useStyles()
     const [isMenuExpanded, setIsMenuExpanded] = useState(false)
     const router = useRouter()
     const path = router.pathname
@@ -30,33 +47,21 @@ const MobileMenu = () => {
             </button>
             <Box className={`${styles.mobileMenu} ${isMenuExpanded ? styles.show : ''}`}>
                 <Box className={styles.mobileNavListContainer}>
-                    <Link href="/">
-                        <a
-                            className={`${styles.mobileNavItem} ${
-                                path === '/' ? styles.active : ''
-                            }`}
-                        >
-                            HOME
-                        </a>
-                    </Link>
-                    <Link href="/usmf-stake">
-                        <a
-                            className={`${styles.mobileNavItem} ${
-                                path === '/usmf-stake' ? styles.active : ''
-                            }`}
-                        >
-                            USMF STAKE
-                        </a>
-                    </Link>
-                    <Link href="/all-network">
-                        <a
-                            className={`${styles.mobileNavItem} ${
-                                path === '/lp-stake' ? styles.active : ''
-                            }`}
-                        >
-                            LP STAKE
-                        </a>
-                    </Link>
+                    <Button className={`${path === '/' ? classes.active : ''}`}>
+                        <Link href="/">
+                            <a className={classes.mobileNavItem}>HOME</a>
+                        </Link>
+                    </Button>
+                    <Button className={`${path === '/usmf-stake' ? classes.active : ''}`}>
+                        <Link href="/usmf-stake">
+                            <a className={classes.mobileNavItem}>USMF STAKE</a>
+                        </Link>
+                    </Button>
+                    <Button className={`${path === '/lp-stake' ? classes.active : ''}`}>
+                        <Link href="/lp-stake">
+                            <a className={classes.mobileNavItem}>LP STAKE</a>
+                        </Link>
+                    </Button>
                 </Box>
                 <MobileAccountButton />
             </Box>
