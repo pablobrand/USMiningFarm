5583eac5213a5fb23b4ebf0d14c8246dba9ae740
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2021-07-08 20:56:42 +0800

    changes

---
+++ functions/utils/addressTruncator.js
@@ -0,0 +1,5 @@
+const addressTruncator = (addr = '') => {
+    return addr.slice(0, 6)
+}
+
+export default addressTruncator
---
+++ functions/utils/index.js
@@ -0,0 +1,4 @@
+import addressTruncator from './addressTruncator'
+
+// eslint-disable-next-line import/prefer-default-export
+export { addressTruncator }
